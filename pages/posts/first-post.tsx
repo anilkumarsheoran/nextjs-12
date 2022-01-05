d6b602edc56313e16d6f2140238c152ca1137aba
Anil Sheoran <anilkumar.sheoran@gmail.com>
2022-01-05 14:07:56 +0530

    adding typescript and first image

--- pages/posts/first-post.tsx
+++ pages/posts/first-post.tsx
@@ -1,8 +1,16 @@
+// @ts-check
 import Link from 'next/link'
+import Image from 'next/image'
 
 export default function FirstPost() {
     return (
         <>
+             <Image
+                src="/images/profile.jpeg"
+                height={144}
+                width={144}
+                alt="Your Name"
+            />
             <h1>first post</h1>
             <Link href="/">
                 <a> Back to home page</a>
