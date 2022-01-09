22225be6c9f0f9dabe59b079ec0d0722307ba855
Anil Sheoran <anilkumar.sheoran@gmail.com>
2022-01-09 18:36:43 +0530

    adding header component and dark mode

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,5 +1,9 @@
 import styles from './layout.module.css'
+import Header from '../components/header'
 
 export default function Layout({ children }){
-    return <div className= { styles.container }>{children}</div>
+    return <div className= "md:container md:mx-auto">
+        <Header />
+        {children}
+        </div>
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
+import 'tailwindcss/tailwind.css'
 import '../styles/globals.css'
+import Layout from '../components/layout'
+import { ThemeProvider } from "next-themes";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider defaultTheme="light" attribute="class">
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
