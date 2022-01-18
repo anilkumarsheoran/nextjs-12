e0cfd7b05997b4d7ec6950420cf1f4512b6b1d11
System Administrator <anilkumar.sheoran@gmail.com>
2022-01-18 22:24:48 +0530

    adding initial footer component

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,9 +1,11 @@
 import styles from './layout.module.css'
 import Header from '../components/header'
+import Footer from '../components/footer'
 
 export default function Layout({ children }){
     return <div className= "md:container md:mx-auto">
         <Header />
         {children}
+        <Footer />
         </div>
 }
